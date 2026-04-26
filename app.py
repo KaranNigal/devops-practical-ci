8701397e8796028ac3ced054500cae0b0973688e
Karan Nigal <karannigal35@gmail.com>
2026-04-26 12:41:40 +0530

    Initial CI project with tests

---
+++ app.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+def is_even(n):
+    return n % 2 == 0
+
+
+if __name__ == "__main__":
+    print("Add:", add(10, 5))
+    print("Subtract:", subtract(10, 5))
+    print("Multiply:", multiply(10, 5))
+    print("Divide:", divide(10, 5))
+    print("Is Even:", is_even(4))
